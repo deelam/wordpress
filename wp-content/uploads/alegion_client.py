4827fce29f06e44fa10ca23f1229efb2a6ea1ce2
deelam <deelam@users.noreply.github.com>
2019-07-23 11:18:23 -0500

    UPDATE wp-content/uploads/alegion_client.py

--- wp-content/uploads/alegion_client.py
+++ wp-content/uploads/alegion_client.py
@@ -11,8 +11,8 @@ Use the 'cred' action again to override the file if your credentials change.
 For debugging purposes, the alegion-cred.json file is not encrypted so the password is visible.
 Delete the alegion-cred.json file at the end of your session if desired.
 
-When the access_token is expired, authentication will be automatically performed, 
-and the alegion-auth.json file will be updated with the authentication response 
+When the access_token is expired, authentication will be automatically performed,
+and the alegion-auth.json file will be updated with the authentication response
 from the server and will be used for all actions.
 
 Author: Dung Lam
@@ -69,7 +69,7 @@ def get_auth_token(realm, un, pwd):
         new_token = jsonresponse["access_token"]
         return new_token
     else:
-        print("Oops, I got this response when asking for an auth token\n",
+        print("Error when asking for an access_token\n",
             response.status_code, response.text)
         sys.exit(1)
 
@@ -77,7 +77,7 @@ def load_auth_token():
     if not os.path.exists(authfile):
         return None
     fileage=time.time()-os.stat(authfile).st_mtime
-    print("access_token age:", fileage)
+    #print("  access_token age:", fileage)
     with open(authfile) as jsonfile:
         data=json.load(jsonfile)
         if fileage>data["expires_in"]:
@@ -87,7 +87,7 @@ def load_auth_token():
 
 ##===================== API requests ======================================
 
-def request_and_print_response(req_type, url, headers={}, bodydata=None, files=None):
+def request_and_print_response(req_type, url, headers={}, bodydata=None, files=None, pageSize=None, pageNum=None, sort=None):
     if "authorization" not in headers:
         headers["authorization"]="Bearer %s" % token
     if "cache-control" not in headers:
@@ -96,21 +96,36 @@ def request_and_print_response(req_type, url, headers={}, bodydata=None, files=N
         headers["content-type"]="application/json"
     #headers["accepts"]="text/csv"
 
-    print(url)
+    print("-------------------------------------------------")
+    print("Request URL: ", url);
     #print("  headers:", headers)
+    reqParams={};
+    if(pageSize):
+        reqParams['pageSize']=pageSize;
+    if(pageNum):
+        reqParams['page']=pageNum;
+    if(sort):
+        reqParams['sort']=sort;
+    if(len(reqParams)>0):
+        print("  paginating with pageSize:",pageSize," pageNum:",pageNum," sort:",sort)
 
-    response = requests.request(req_type, url, headers=headers, data=bodydata, files=files)
+    response = requests.request(req_type, url, headers=headers, params=reqParams, data=bodydata, files=files)
     # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
     if response.status_code >= 300:
         print("Error: ", response, response.text)
         sys.exit(1)
     else:
+        print("-------------------------------------------------")
+        #  https://stackoverflow.com/questions/24533018/get-a-header-with-python-and-convert-in-json-requests-urllib2-json
+        print("Response headers:", json.dumps(dict(response.headers), indent=2))
+        print("-------------------------------------------------")
         print("Response:", json.dumps(json.loads(response.content),
             indent=2, sort_keys=True))
 
-def list_workflows():
+def list_workflows(pageSize=None, pageNum=None, sort=None):
     request_and_print_response("GET",
-        alegion_baseurl+"/workflows")
+        alegion_baseurl+"/workflows",
+        pageSize=pageSize, pageNum=pageNum, sort=sort)
 
 def list_batches(workflowId):
     request_and_print_response("GET",
@@ -169,13 +184,15 @@ def add_records_csv(batchId, csv_filename):
             alegion_baseurl+"/batches/{}/records/import-csv".format(batchId),
             files=dict(file=csvfile))
 
-def list_records(batchId):
+def list_records(batchId, pageSize=None, pageNum=None, sort=None):
     request_and_print_response("GET",
-        alegion_baseurl+"/batches/{}/records".format(batchId))
+        alegion_baseurl+"/batches/{}/records".format(batchId),
+        pageSize=pageSize, pageNum=pageNum, sort=sort)
 
-def get_results(batchId):
+def get_results(batchId, pageSize=None, pageNum=None, sort=None):
     request_and_print_response("GET",
-        alegion_baseurl+"/batches/{}/results".format(batchId))
+        alegion_baseurl+"/batches/{}/results".format(batchId),
+        pageSize=pageSize, pageNum=pageNum, sort=sort)
 
 ##======================== Main program ==================================
 
@@ -200,19 +217,21 @@ actions={
 }
 
 help_text="""Following are possible actions, their parameters, and what each action does.
-    cred realm username password          : set credentials in the {} file
-    workflows                             : list your workflows
-    batches workflowId                    : list batches in the specified workflow
-    createBatch workflowId name           : create a new batch with given name in the specified workflow
-    createBatchJson workflowId jsonString : create a new batch given as jsonString within the specified workflow
-    batch batchId                         : get information about the specified batch
-    editBatch batchId jsonString          : edit information about the specified batch
-    addRecord batchId jsonString          : add a new record given as jsonString to the specified batch
-    addRecords batchId jsonArrayString    : add new records given as jsonArrayString to the specified batch
-    addRecordsJson batchId jsonFile       : add new records specified in jsonFile to the specified batch
-    addRecordsCsv batchId csvFile         : add new records specified in csvFile to the specified batch
-    records batchId                       : list records in the specified batch
-    results batchId                       : show results associated with the specified batch\
+    cred realm username password                : set credentials in the {} file
+    workflows [pageSize] [pageNum] [sort]       : list your workflows
+    batches workflowId                          : list batches in the specified workflow
+    createBatch workflowId name                 : create a new batch with given name in the specified workflow
+    createBatchJson workflowId jsonString       : create a new batch given as jsonString within the specified workflow
+    batch batchId                               : get information about the specified batch
+    editBatch batchId jsonString                : edit information about the specified batch
+    addRecord batchId jsonString                : add a new record given as jsonString to the specified batch
+    addRecords batchId jsonArrayString          : add new records given as jsonArrayString to the specified batch
+    addRecordsJson batchId jsonFile             : add new records specified in jsonFile to the specified batch
+    addRecordsCsv batchId csvFile               : add new records specified in csvFile to the specified batch
+    records batchId [pageSize] [pageNum] [sort] : list records in the specified batch
+    results batchId [pageSize] [pageNum] [sort] : show results associated with the specified batch
+
+Some actions produce a lot of output. Use the optional pageSize, pageNum, and sort parameters to control pagination.
 """.format(credfile)
 
 parser = argparse.ArgumentParser(description="Basic commandline tool for testing Alegion API endpoints.",
@@ -223,11 +242,11 @@ parser.add_argument('action', help="action to perform")
 #choices=actions.keys())
 parser.add_argument('actionparam', nargs="*", help="action parameters")
 # parse command line parameters
-args = parser.parse_args()
+args, unknown = parser.parse_known_args() # ignore unknown args
 
 action = args.action
-actionparams = args.actionparam
-#print("args: ", args)
+actionparams = args.actionparam + unknown
+#print("args: ", args, "unknown", unknown)
 
 if action not in actions.keys():
     print("!! Unknown action:", action)
